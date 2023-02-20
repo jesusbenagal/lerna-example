eaa940e2d163cae2b95fd6cde8fde78f772687e9
Jesús Guerrero <jesus.guerrero@talan.com>
2023-02-20 13:06:02 +0100

    Adding Table component

---
+++ packages/table/src/components/Table.jsx
@@ -0,0 +1,57 @@
+import { useMemo } from "react";
+import { useTable } from "react-table";
+import "../styles/table.css";
+
+const BasicTable = ({ columnsTable, dataTable }) => {
+  const columns = useMemo(() => columnsTable, []);
+  const data = useMemo(() => dataTable, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        {footerGroups.map((footerGroup) => (
+          <tr {...footerGroup.getFooterGroupProps()}>
+            {footerGroup.headers.map((column) => (
+              <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </table>
+  );
+};
+
+export default BasicTable;
